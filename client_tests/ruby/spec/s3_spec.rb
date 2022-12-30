880a44b5c9cbba67c6d5c13627717404d2b0c532
Andrei Zavada <andrei.zavada@tiot.jp>
2022-12-30 20:30:17 +0200

    upgrade ruby test to aws-sdk "~>3"

--- client_tests/ruby/spec/s3_spec.rb
+++ client_tests/ruby/spec/s3_spec.rb
@@ -23,97 +23,121 @@ require 'uuid'
 require 'yaml'
 require 'helper'
 
-class AWS::S3::AccessControlList::Grant
+class Aws::S3Control::Types::S3AccessControlList::Grant
   def  ==(other)
     @grantee.to_s == other.grantee.to_s and @permission.to_s == other.permission.to_s
   end
 end
 
-describe AWS::S3 do
-  let(:s3) { AWS::S3.new( s3_conf ) }
+describe Aws::S3 do
+  let(:s3) { Aws::S3::Client.new( s3_conf ) }
   let(:bucket_name) { "aws-sdk-test-" + UUID::generate }
   let(:object_name) { "key-" + UUID::generate }
-  let(:grant_public_read) {
-    acl = AWS::S3::AccessControlList.new
-    acl.grant(:read).to(:uri => 'http://acs.amazonaws.com/groups/global/AllUsers')
-    acl.grants.first
-  }
 
   after :each do
-    begin
-      if s3.buckets[bucket_name].exists?
-        if s3.buckets[bucket_name].objects.count > 0
-          s3.buckets[bucket_name].objects.each &:delete
-        end
-        s3.buckets[bucket_name].delete
-      end
-    rescue Exception => e
-      puts e
-    end
+    s3.list_buckets({}).buckets
+      .each { |b| s3.list_objects({bucket: b.name}).contents
+                .each { |o| s3.delete_object({bucket: b.name, key: o.key}) }
+      s3.delete_bucket({bucket: b.name})
+    }
+  rescue Exception => e
+    puts e
   end
 
   describe "when there is no bucket" do
     it "should not find the bucket." do
-      s3.buckets[bucket_name].exists?.should == false
+      bb = s3.list_buckets({})
+      bb.include?(bucket_name) == false
     end
 
     it "should fail on delete operation." do
-      lambda{s3.buckets[bucket_name].delete}.should raise_error
+      expect(lambda{s3.delete_bucket({bucket: bucket_name})})
+        .to raise_error Aws::S3::Errors::NoSuchBucket
     end
 
     it "should fail on get acl operation" do
-      lambda{s3.buckets[bucket_name].acl}.should raise_error
+      expect(lambda{s3.get_bucket_acl({bucket: bucket_name})})
+        .to raise_error Aws::S3::Errors::NoSuchBucket
     end
   end
 
   describe "when there is a bucket" do
     it "should be able to create and delete a bucket" do
-      s3.buckets.create(bucket_name).should be_kind_of(AWS::S3::Bucket)
-      s3.buckets[bucket_name].exists?.should == true
+      s3.create_bucket({bucket: bucket_name})
+      bb = s3.list_buckets({})
+      bb.include?(bucket_name) == true
 
-      lambda{s3.buckets[bucket_name].delete}.should_not raise_error
-      s3.buckets[bucket_name].exists?.should == false
+      lambda{s3.delete_bucket({bucket: bucket_name}).should_not raise_error}
+      s3.list_buckets({}).include?(bucket_name) == false
     end
 
     it "should be able to list buckets" do
-      s3.buckets.create(bucket_name).should be_kind_of(AWS::S3::Bucket)
-      s3.buckets.count.should > 0
+      s3.create_bucket({bucket: bucket_name})
+      expect(s3.list_buckets({}).buckets.any? { |b| b.name == bucket_name }).to be true
     end
 
     it "should be able to put, get and delete object" do
-      s3.buckets.create(bucket_name).should be_kind_of(AWS::S3::Bucket)
+      expect(s3.create_bucket({bucket: bucket_name}).data)
+        .to be_kind_of(Aws::S3::Types::CreateBucketOutput)
 
-      s3.buckets[bucket_name].objects[object_name].write('Rakefile')
-      s3.buckets[bucket_name].objects[object_name].exists?.should == true
+      expect(s3.put_object({bucket: bucket_name,
+                            key: object_name,
+                            body: 'Rakefile'}).data)
+        .to be_kind_of(Aws::S3::Types::PutObjectOutput)
+      expect(s3.get_object({bucket: bucket_name,
+                            key: object_name}).body.read())
+        .to eql('Rakefile')
 
-      s3.buckets[bucket_name].objects[object_name].read.should == 'Rakefile'
-
-      s3.buckets[bucket_name].objects[object_name].delete
-      s3.buckets[bucket_name].objects[object_name].exists?.should == false
+      expect(s3.delete_object({bucket: bucket_name,
+                               key: object_name}).data)
+        .to be_kind_of(Aws::S3::Types::DeleteObjectOutput)
+      expect(s3.list_objects({bucket: bucket_name}).contents)
+        .to be_empty
     end
 
     it "should be able to put and get bucket ACL" do
-      s3.buckets.create(bucket_name, :acl => :public_read).should be_kind_of(AWS::S3::Bucket)
-      s3.buckets[bucket_name].acl.grants.include?(grant_public_read).should == true
+      expect(s3.create_bucket({bucket: bucket_name, acl: "public-read"}).data)
+        .to be_kind_of(Aws::S3::Types::CreateBucketOutput)
+      expect(s3.get_bucket_acl({bucket: bucket_name}).data.grants[0].to_h)
+        .to eql({:grantee=>{:type=>"Group",
+                            :uri=>"http://acs.amazonaws.com/groups/global/AllUsers"},
+                 :permission=>"READ"})
     end
 
     it "should be able to put and get object ACL" do
-      s3.buckets.create(bucket_name).should be_kind_of(AWS::S3::Bucket)
-      s3.buckets[bucket_name].objects[object_name].write('Rakefile', :acl => :public_read)
+      expect(s3.create_bucket({bucket: bucket_name}).data)
+        .to be_kind_of(Aws::S3::Types::CreateBucketOutput)
+      expect(s3.put_object({bucket: bucket_name,
+                            key: object_name,
+                            acl: "public-read",
+                            body: 'Rakefile'}).data)
+        .to be_kind_of(Aws::S3::Types::PutObjectOutput)
 
-      s3.buckets[bucket_name].objects[object_name].acl.grants.include?(grant_public_read).should == true
+      expect(s3.get_bucket_acl({bucket: bucket_name}).data.grants[0].to_h)
+        .to eql({:grantee=>{:display_name=>"admin",
+                            :id=>ENV['USER_ID'],
+                            :type=>"CanonicalUser"},
+                 :permission=>"FULL_CONTROL"})
     end
 
     it "should be able to put object using multipart upload" do
-      s3.buckets.create(bucket_name).should be_kind_of(AWS::S3::Bucket)
+      s3.create_bucket({bucket: bucket_name})
 
-      temp = new_mb_temp_file 6 # making 6MB file
-      s3.buckets[bucket_name].objects[object_name].write(
-                                    :file => temp.path,
-                                    :multipart_threshold => 1024 * 1024,
-                                   )
-      s3.buckets[bucket_name].objects[object_name].exists?.should == true
-      temp.close
+      Tempfile.create('riakcs-test') do |f1|
+        size = 1
+        (size*1024/2).times {|i| f1.write "fa"}
+        puts "putting six!"
+        File.open(f1.path, 'rb') do |f2|
+          s3.put_object({bucket: bucket_name,
+                         key: object_name,
+                         acl: "public-read",
+                         body: f2})
+        end
+      end
+
+      expect(s3.list_objects({bucket: bucket_name}).contents)
+        .to satisfy {|a| a.any? { |o| o.key == object_name }
+      }
     end
   end
 end
