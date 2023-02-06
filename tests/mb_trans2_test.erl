f3a29fb6bd4eac094f32a25b73b4fba106cfe4d3
Andrei Zavada <andrei.zavada@tiot.jp>
2023-02-06 19:14:18 +0200

    incremental fix of copyright notices

--- tests/mb_trans2_test.erl
+++ tests/mb_trans2_test.erl
@@ -1,4 +1,23 @@
-%% Copyright (c) 2015 Basho Technologies, Inc.  All Rights Reserved.
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2015 Basho Technologies, Inc.
+%%               2021-2023 TI Tokyo    All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
 
 -module(mb_trans2_test).
 
@@ -51,7 +70,7 @@ transition_to_mb(NodesInMaster, State) ->
     CSNodes = cs_suites:nodes_of(cs, State),
     BagConf = rtcs_bag:conf(NodesInMaster, disjoint),
 
-    rt:pmap(fun(N) -> rtcs_dev:stop(N) end, CSNodes),
+    rt:pmap(fun rtcs_dev:stop/2, CSNodes),
     %% Because there are noises from poolboy shutdown at stopping riak-cs,
     %% truncate error log here and re-assert emptiness of error.log file later.
     rtcs_dev:truncate_error_log(1),
